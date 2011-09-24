7aeb49073331da8e7bd639070ab19283fe41419f
Takumi <takumi.aki@gmail.com>
2011-09-25 03:25:08 +0900

    機能をモジュールに追い出し （CSVHack.pm）

--- make_csv.pl
+++ make_csv.pl
@@ -3,49 +3,22 @@
 use strict;
 use warnings;
 
-use lib "$ENV{HOME}/perl-utils/";
-
-use Text::CSV;
-use IO::File;
-use File::Copy::Recursive qw/rcopy rmove/;
-use autodie qw/:default rcopy rmove/;
-use utf8;
+use CSVHack;
 
 my $filename   = '13tokyo.csv';
 my $backup_dir = './back/';
 my $tmp_dir    = './tmp/';
 
-my $read_csv = Text::CSV->new(
+my $csv = CSVHack->new(
     {
-        auto_diag => 1,
-        binary    => 1,
+        filename   => $filename,
+        backup_dir => $backup_dir,
+        tmp_dir    => $tmp_dir,
     }
 );
-
-open( my $read_fh, '<:encoding(cp932)', $filename );
-
-my @file;
-while ( my $row = $read_csv->getline($read_fh) ) {
-    push @file, $row;
-}
-
-close $ifh;
-
-# backup
-rcopy( $filename, $backup_dir );
 
 my $new_row = [ 0000, 14, 11111 ];
 
-push @file, $new_row;
+$csv->add_row($new_row);
 
-my $write_csv = Text::CSV->new(
-    {
-        auto_diag => 1,
-        binary    => 1,
-        eol       => "\r\n",
-    }
-);
-
-open( my $write_fh, '>:encoding(cp932)', $filename );
-
-$write_csv->print( $write_fh, $_ ) for @file;
+$csv->backup->save;
